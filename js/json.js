9e32475c1a4a4171a66b6da9d5a6971f63eb2842
zackw22 <zackw22@gmail.com>
2012-09-24 09:44:00 -0600

    added branch

---
+++ js/json.js
@@ -0,0 +1,39 @@
+var json = {
+    "workout1": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "220"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "arms"],
+       "reps": ["Reps:", "12"],
+       "comments": ["Comments:","Arms days"]
+    },
+    "workout2": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "215"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "legs"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Lower body days"]
+    },
+     "workout3": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "210"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "cardio"],
+       "reps": ["Reps:", "1"],
+       "comments": ["Comments:","Cardio days"]
+    },
+      "workout4": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "210"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "back"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Back getting ripped"]
+    }
+    
+}
